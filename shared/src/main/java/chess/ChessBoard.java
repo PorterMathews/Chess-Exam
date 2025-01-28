0b44abc8efe16a120aa8722b1e333a97fa47ea19
Porter Mathews <porterlikesbacon@gmail.com>
2025-01-27 20:24:50 -0700

    The whole project

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +10,24 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private final ChessPiece[][] chessBoard = new ChessPiece[9][9];
 
     public ChessBoard() {
-        
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(chessBoard, that.chessBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(chessBoard);
     }
 
     /**
@@ -19,7 +37,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        chessBoard[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +48,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return chessBoard[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +56,117 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (int col = 0; col <= 8; col++) {
+            for (int row = 0; row <= 8; row++) {
+                chessBoard[row][col] = null;
+            }
+        }
+
+        int column = 1;
+        while (column <= 8) {
+            addPiece(new ChessPosition(2, column), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7, column), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+            column++;
+        }
+
+        //WhitePieces
+        addPiece(new ChessPosition(1, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(1, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(1, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
+        //BlackPieces
+        addPiece(new ChessPosition(8, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
     }
+
+    /**
+     *
+     * @param checkPosition The position you are checking
+     * @return True if in bounds
+     */
+    public boolean isInBounds(ChessPosition checkPosition) {
+        if (checkPosition.getColumn() > 8 || checkPosition.getRow() > 8 ||
+            checkPosition.getColumn() < 1 || checkPosition.getRow() < 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param checkPosition The position the potential other piece is at
+     * @param myPiece The piece you are attempting to move to checkPosition
+     * @return True if the piece is a different color or if the space is empty
+     */
+    public boolean isOccupiedByEnemy(ChessPosition checkPosition, ChessPiece myPiece) {
+        if (getPiece(checkPosition) == null) {
+            return true;
+        }
+        ChessPiece occupiedPiece = getPiece(checkPosition);
+        return occupiedPiece.getTeamColor() != myPiece.getTeamColor();
+    }
+
+    /**
+     *
+     * @param checkPosition The position you want to know if it is occupied
+     * @return returns true if occupied
+     */
+    public boolean isOccupied(ChessPosition checkPosition) {
+        return getPiece(checkPosition) != null;
+    }
+
+    /**
+     *
+     * @param checkPosition The position you are checking
+     * @param myPiece The piece you are attempting to move
+     * @return ture if the square is on the board and the checkPosition is empty or has an enemy piece
+     */
+    public boolean isValidMove(ChessPosition checkPosition, ChessPiece myPiece) {
+        return isInBounds(checkPosition) && isOccupiedByEnemy(checkPosition, myPiece);
+    }
+
+    /**
+     *
+     * @param checkPosition The position you are checking
+     * @param myPiece The piece you are attempting to move
+     * @return Ture only if the space is occupied by an enemy piece, else false
+     */
+    public boolean pawnIsOccupiedByEnemy(ChessPosition checkPosition, ChessPiece myPiece) {
+        if (getPiece(checkPosition) == null) {
+            return false;
+        }
+        ChessPiece occupiedPiece = getPiece(checkPosition);
+        return occupiedPiece.getTeamColor() != myPiece.getTeamColor();
+    }
+
+    /**
+     *
+     * @param checkPosition The position you are trying to move to
+     * @return true if the position is in bounds and is not occupied
+     */
+    public boolean isValidPawnMove(ChessPosition checkPosition) {
+        return isInBounds(checkPosition) && !isOccupied(checkPosition);
+    }
+
+    /**
+     *
+     * @param checkPosition The position you are attempting to attack
+     * @param myPiece The piece you are attacking with, pawn
+     * @return true if the position is in bounds and is occupied by an enemy
+     */
+    public boolean isValidPawnAttack(ChessPosition checkPosition, ChessPiece myPiece) {
+        return isInBounds(checkPosition) && pawnIsOccupiedByEnemy(checkPosition, myPiece);
+    }
+
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +11,26 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -29,14 +49,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +67,35 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> pieceMoves = new ArrayList<>();
+
+        switch(type) {
+            case KING:
+                KingMovesCalc kingMovesCalc = new KingMovesCalc(board, myPosition);
+                return kingMovesCalc.pieceMoves(board, myPosition);
+
+            case QUEEN:
+                QueenMoveCalc queenMoveCalc = new QueenMoveCalc(board, myPosition);
+                return queenMoveCalc.pieceMoves(board, myPosition);
+
+            case ROOK:
+                RookMoveCalc rookMoveCalc = new RookMoveCalc(board, myPosition);
+                return rookMoveCalc.pieceMoves(board, myPosition);
+
+            case KNIGHT:
+                KnightMovesCalc knightMovesCalc = new KnightMovesCalc(board, myPosition);
+                return knightMovesCalc.pieceMoves(board, myPosition);
+
+            case BISHOP:
+                BishopMoveCalc bishopMoveCalc = new BishopMoveCalc(board, myPosition);
+                return bishopMoveCalc.pieceMoves(board, myPosition);
+
+            case PAWN:
+                PawnMoveCalc pawnMoveCalc = new PawnMoveCalc(board, myPosition);
+                return pawnMoveCalc.pieceMoves(board, myPosition);
+
+            default:
+                throw new RuntimeException("Not a valid Piece");
+        }
     }
 }
